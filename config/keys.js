7c6cae533f3b2144d06ebb1bab0a76025e3c3a5d
kamkanev <kamkanev@gmail.com>
2020-06-16 11:59:46 +0300

    Make db and side run again

---
+++ config/keys.js
@@ -0,0 +1,12 @@
+module.exports = {
+  google:{
+    clientID: "311628842870-f5o6kc2afsefnr7top74nmh2j1ukm3a7.apps.googleusercontent.com",
+    clientSecret: "RTXnXhtAfWpLben3zNgUmdFH"
+  },
+  mongodb: {
+    dbURI: "mongodb://admin:password1@ds263137.mlab.com:63137/rebook-app"
+  },
+  cookie:{
+    secret: "*V84thbnvf6tYI+)U3Td324hHJ"
+  }
+};
