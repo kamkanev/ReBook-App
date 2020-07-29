08257541b8ef118d2f3e3a9cca859dc6bf177419
kamkanev <kamkanev@gmail.com>
2020-07-29 17:25:55 +0300

    fixxed notebook style/ added bgs + founded new bugs

--- js/ebooks.js
+++ js/ebooks.js
@@ -22,10 +22,25 @@ function toggleEdit() {
   if(isInEditMode){
     textEditor.document.designMode = 'Off';
     isInEditMode = false;
+    Swal.fire({
+    position: 'top-end',
+    icon: 'success',
+    title: 'Файла вече може да се редактира сега!',
+    showConfirmButton: false,
+    timer: 2000
+  })
   }else{
     textEditor.document.designMode = 'On';
     isInEditMode = true;
+    Swal.fire({
+    position: 'top-end',
+    icon: 'success',
+    title: 'Файла не може да се редактира сега!',
+    showConfirmButton: false,
+    timer: 2000
+  })
   }
+
 }
 
 function changeBackground(val) {
@@ -33,18 +48,45 @@ function changeBackground(val) {
   console.log(val);
 
    if(val == 0){
-     textEditor.document.style = "";
+     document.getElementById('textEditor').style = "";
    }else if(val == 1){
-    textEditor.document.style.cssText = "#textEditor {"+
-     "background: url(http://i.stack.imgur.com/ynxjD.png) repeat-y;"+
+    document.getElementById('textEditor').style.cssText = "background: url('../img/books/backgrounds/bgBook1.png') repeat-y;"+
      "line-height: 25px;"+
      "padding: 2px 10px;"+
-     "border: solid 1px #ddd;"+
-    "}";
+     "display: block;"+
+     "resize: none;"+
+     "height: 800px;"+
+     "width: 630px;"+
+     "margin: 2% auto;"+
+     "-webkit-background-size:  100% 50px;"+
+     "background-size: 100% 50px;"+
+     "border: solid 1px #ddd;";
   }else if(val == 2){
-    textEditor.document.style = "#textEditor"+
-    "{"+
-        "border: 1px solid #EEEEEE;"+
+    document.getElementById('textEditor').style.cssText = "background: url('../img/books/backgrounds/bgBook2.png') repeat-y;"+
+     "line-height: 25px;"+
+     "padding: 2px 10px;"+
+     "display: block;"+
+     "resize: none;"+
+     "height: 800px;"+
+     "width: 630px;"+
+     "margin: 2% auto;"+
+     "-webkit-background-size:  100% 50px;"+
+     "background-size: 100% 50px;"+
+     "border: solid 1px #ddd;";
+  }else if(val == 3){
+    document.getElementById('textEditor').style.cssText = "background: url('../img/books/backgrounds/bgBook3.png') repeat-y;"+
+     "line-height: 25px;"+
+     "padding: 2px 10px;"+
+     "display: block;"+
+     "resize: none;"+
+     "height: 800px;"+
+     "width: 630px;"+
+     "margin: 2% auto;"+
+     "-webkit-background-size:  100% 50px;"+
+     "background-size: 100% 50px;"+
+     "border: solid 1px #ddd;";
+  }else if(val == 4){
+    document.getElementById('textEditor').style = "border: 1px solid #EEEEEE;"+
         "box-shadow: 1px 1px 0 #DDDDDD;"+
         "display: block;"+
         "font-family: 'Marck Script',cursive;"+
@@ -53,13 +95,12 @@ function changeBackground(val) {
         "margin: 2% auto;"+
         "padding: 11px 20px 0 70px;"+
         "resize: none;"+
-        "height: 390px;"+
-        "width: 530px;"+
+        "height: 800px;"+
+        "width: 630px;"+
         "background-image: -moz-linear-gradient(top , transparent, transparent 49px,#E7EFF8 0px), -moz-radial-gradient(4% 50%, circle closest-corner, #f5f5f5, #f5f5f5 39%, transparent 0%), -moz-radial-gradient(3.9% 46% , circle closest-corner, #CCCCCC, #CCCCCC 43.5%, transparent 0%);"+
         "background-image: -webkit-linear-gradient(top , transparent, transparent 49px,#E7EFF8 0), -webkit-radial-gradient(4% 50%, circle closest-corner, #f5f5f5, #f5f5f5 39%, transparent 0%), -webkit-radial-gradient(3.9% 46%, circle closest-corner, #cccccc, #cccccc 43.5%, transparent 0%);"+
         "-webkit-background-size:  100% 50px;"+
-        "background-size: 100% 50px;"+
-    "}";
+        "background-size: 100% 50px;";
   }
 
 }
