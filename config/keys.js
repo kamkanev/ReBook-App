d0b9bbecf9b70927366e6485e291cfa79f4d16ee
kamkanev <kamkanev@gmail.com>
2020-09-04 18:34:04 +0300

    update key and migrate db

--- config/keys.js
+++ config/keys.js
@@ -4,7 +4,8 @@ module.exports = {
     clientSecret: "RTXnXhtAfWpLben3zNgUmdFH"
   },
   mongodb: {
-    dbURI: "mongodb://admin:password1@ds263137.mlab.com:63137/rebook-app"
+    // dbURI: "mongodb://admin:password1@ds263137.mlab.com:63137/rebook-app"
+    dbURI: "mongodb+srv://admin:password1@rebook-app.d6koz.mongodb.net/rebook-app?retryWrites=true&w=majority"
   },
   cookie:{
     secret: "*V84thbnvf6tYI+)U3Td324hHJ"
